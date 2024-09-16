9064dc6c7bb8eaa7a2331435439b10d32ba4ba67
cameronpool <cameronpool2019@gmail.com>
2024-09-16 11:03:50 -0500

    removed initial files that vite creates that were not needed

---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+
+
+function Navbar() {
+    return (
+        <div>
+            <nav>
+                <h1 className="nav">todo</h1>
+            </nav>
+        </div>
+    )
+}
+
+export default Navbar;
