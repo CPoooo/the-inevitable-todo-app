1e17e513bd9bb86619b85693764db203255862ff
cameronpool <cameronpool2019@gmail.com>
2024-09-17 11:56:45 -0500

    initial UI setup

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,9 +1,13 @@
 function Footer() {
 
+  const date = new Date();
+  let year = date.getFullYear();
+
     return (
-      <div>
-          <h1 className="footer">footer</h1>
-      </div>
+      <footer>
+          <p className="copyright">Copyright © {year} Cameron Pool</p>
+          <button className="contact">Contact Me</button>
+      </footer>
     );
   }
   
