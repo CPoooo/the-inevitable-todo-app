0efeab8a7a339af1659d356eff7c993066f8b32c
cameronpool <cameronpool2019@gmail.com>
2024-09-17 11:45:14 -0500

    better hero text, more designs to come though

--- frontend/src/components/Hero.jsx
+++ frontend/src/components/Hero.jsx
@@ -4,9 +4,9 @@ function Hero() {
   return (
     <div className="mainContainer">
       <div className="hero">
-        <h1 className="heroTitle">OMG! Another Todo App?</h1>
-        <h2 className="heroSubTitle">Anyways... New here? Get started below</h2>
-        <p className="heroText">Returning members sign in up top</p>
+        <h1 className="heroTitle">todoApp</h1>
+        <h2 className="heroSubTitle">New here? Get started below</h2>
+        {/* <p className="heroText">Returning members sign in up top</p> */}
         <RegisterButtons />
       </div>
     </div>
